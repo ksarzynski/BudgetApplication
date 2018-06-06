0fc58c8fd75a3cac0efe3782a306d5200a77f95b
projektinfug <33053989+projektinfug@users.noreply.github.com>
2018-06-06 15:21:30 +0200

    Update ScannerController.cs

--- BudgetApplication/Controllers/ScannerController.cs
+++ BudgetApplication/Controllers/ScannerController.cs
@@ -71,27 +71,11 @@ namespace BudgetApplication.Controllers
             return View();
         }
 
-
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+     
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
