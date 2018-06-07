e2016403fde3af862f82414bae14a492322aded8
projektinfug <33053989+projektinfug@users.noreply.github.com>
2018-06-07 22:43:24 +0200

    Update Tesseract.cs

--- BudgetApplication/Models/Tesseract.cs
+++ BudgetApplication/Models/Tesseract.cs
@@ -11,37 +11,17 @@
          public string Data { get; private set; }
          public Tesseract(string fileName) { Data = ProcessImage(fileName); }
 
-
-
-         public string getText()
-         {
-             return Data;
-         }
-         public IEnumerable<string> seperateStringToArrayByWhiteSpaces(string text)
-         {
-             IEnumerable<string> array = text.Split(' ');
-             return array;
-         }
          private string ProcessImage(string filename)
          {
              List<string> imageToText = new List<string>();
              var solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
              var tesseractPath = solutionDirectory + @"\tesseract-master.1671";
-             var testFiles = Directory.EnumerateFiles(solutionDirectory + @"\samples");
              var image = File.ReadAllBytes(filename);
-             string text = ParseText(tesseractPath, image, "pol");
-             var maxDegreeOfParallelism = Environment.ProcessorCount;
-             Parallel.ForEach(testFiles, new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism },
-                 (fileName) =>
-             {
-                 var imageFile = File.ReadAllBytes(fileName);
-                 text = ParseText(tesseractPath, imageFile, "pol");
-                 imageToText.Add(text);
-             });
+             string text = ParseText(tesseractPath, image);
              return text;
          }
 
-         private static string ParseText(string tesseractPath, byte[] imageFile, params string[] lang)
+         private static string ParseText(string tesseractPath, byte[] imageFile)
          {
              string output = string.Empty;
              var tempOutputFile = Path.GetTempPath() + Guid.NewGuid();
@@ -51,31 +31,25 @@
              {
                  File.WriteAllBytes(tempImageFile, imageFile);
 
-                 ProcessStartInfo info = new ProcessStartInfo();
+            ProcessStartInfo info = new ProcessStartInfo();
                  info.WorkingDirectory = tesseractPath;
                  info.WindowStyle = ProcessWindowStyle.Hidden;
-                info.UseShellExecute = false;
+                 info.UseShellExecute = false;
                  info.FileName = "cmd.exe";
                  info.Arguments =
                      "/c tesseract.exe " +
-                     // Image file.
                     tempImageFile + " " +
-                    // Output file (tesseract add '.txt' at the end)
                      tempOutputFile +
-                     // Languages.
-                     " -l " + string.Join("+", lang);
-
-                 // Start tesseract.
-                Process process = Process.Start(info);
+                     " -l " + string.Join("+", "pol");
+                 Process process = Process.Start(info);
                  process.WaitForExit();
                  if (process.ExitCode == 0)
                  {
-                     // Exit code: success.
                     output = File.ReadAllText(tempOutputFile + ".txt");
                  }
                  else
                  {
-                     throw new Exception("Error. Tesseract stopped with an error code = " + process.ExitCode);
+                     throw new Exception("Error" + process.ExitCode);
                  }
              }
              finally
