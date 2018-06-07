75cbea40a21dde7c0a7c64ace8706a62990dea77
DawidWięcko <dwiecko@sigma.ug.edu.pl>
2018-06-08 01:32:54 +0200

    Refactor of UI login tests

--- BudgetApplicationSeleniumTests/LoginTests.cs
+++ BudgetApplicationSeleniumTests/LoginTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BudgetApplicationSeleniumTests.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -8,73 +9,56 @@ namespace BudgetApplicationSeleniumTests
     [TestClass]
     public class LoginTests
     {
-        IWebDriver driver;
+        private static IWebDriver driver;
         private string pathToDebug = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, AppDomain.CurrentDomain.RelativeSearchPath ?? "");
         private string baseURL = "http://localhost:49771/Account/Login";
+        private LoginPage loginPage = null;
 
         [TestInitialize]
         public void SetUp()
         {
             driver = new ChromeDriver(pathToDebug);
-           
+            loginPage = new LoginPage(driver);
+            driver.Navigate().GoToUrl(baseURL);
         }
 
         [TestMethod]
         public void LoginEmailNotValid()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            var emailField = driver.FindElement(By.Id("Email"));
-            var passwordField = driver.FindElement(By.Id("Password"));
-            emailField.Click();
-            emailField.SendKeys("testtest");
-            passwordField.Click();
+            loginPage.SendTextToField(loginPage.Email, "testtest");
+            loginPage.Password.Click();
 
-            var error = driver.FindElement(By.Id("Email-error")).Text;
+            var error = loginPage.GetEmailErrorText();
 
             StringAssert.Contains(error, "The Email field is not a valid e-mail address.");
-            driver.Quit();
-
         }
-      
 
         [TestMethod]
         public void LoginWithInvalidPassword()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            var emailField = driver.FindElement(By.Id("Email"));
-            var passwordField = driver.FindElement(By.Id("Password"));
-            emailField.Click();
-            emailField.SendKeys("admin@admin.com");
-            passwordField.Click();
-            passwordField.SendKeys("testtest");
-            driver.FindElement(By.XPath("/html/body/div/div/div[1]/section/form/div[5]/button")).Click(); 
+            loginPage.SendTextToField(loginPage.Email, "admin@admin.com");
+            loginPage.SendTextToField(loginPage.Password, "testtest");
+            loginPage.LoginButton.Click();
 
-            var error = driver.FindElement(By.XPath("/html/body/div/div/div[1]/section/form/div[1]/ul/li")).Text;
+            var error = loginPage.GetEmailErrorText();
 
             StringAssert.Contains(error, "Invalid login attempt.");
-            driver.Quit();
         }
 
 
         [TestMethod]
         public void LoginWithCorrectEmailAndPasswordAsAdmin()
         {
+            loginPage.SendTextToField(loginPage.Email, "admin@admin.com");
+            loginPage.SendTextToField(loginPage.Password, "p@sw1ooorD");
+            loginPage.LoginButton.Click();
 
-            driver.Navigate().GoToUrl(baseURL);
-            var email = driver.FindElement(By.Id("Email"));
-            email.Click();
-            email.SendKeys("admin@admin.com");
-
-            var pass = driver.FindElement(By.Id("Password"));
-            pass.Click();
-            pass.SendKeys("p@sw1ooorD\n");
-
-            var admin = driver.FindElement(By.LinkText("Hello admin@admin.com!")).Text;
+            var admin = loginPage.FindElementByLinkText("Hello admin@admin.com!");
 
             StringAssert.Contains(admin, "Hello admin@admin.com!");
-            driver.Quit();
-
         }
 
+        [TestCleanup]
+        public void TearDown() => driver.Quit();
     }
 }
