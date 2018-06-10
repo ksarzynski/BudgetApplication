6e465d3a416edbdad684453576689365d1db41bd
lzynda <lukaszzynda1@gmail.com>
2018-06-11 01:29:20 +0200

    Add Transaction model tests

--- BudgetApplicationTests/Models/TransactionTest.cs
+++ BudgetApplicationTests/Models/TransactionTest.cs
@@ -13,7 +13,7 @@ namespace BudgetApplicationTests.Models
 
             var model = new Transaction
             {
-                Price = 23
+                Price = 23.00
             };
 
             var context = new ValidationContext(model, null, null);
@@ -23,6 +23,78 @@ namespace BudgetApplicationTests.Models
 
 
             Assert.True(valid);
+        }
+
+        [Fact]
+        public void GivenModelTransactionPriceWithNotValidPriceShouldNotValidate()
+        {
+
+            var model = new Transaction
+            {
+                Price = 23.001
+            };
+
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            var valid = Validator.TryValidateObject(model, context, result, true);
+
+
+            Assert.False(valid);
+        }
+
+        [Fact]
+        public void GivenModelTransactionPriceWithNotValidPriceShouldNotValidate2()
+        {
+
+            var model = new Transaction
+            {
+                Price = -1
+            };
+
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            var valid = Validator.TryValidateObject(model, context, result, true);
+
+
+            Assert.False(valid);
+        }
+
+        [Fact]
+        public void GivenValidModelTransactionPlaceShouldValidate()
+        {
+
+            var model = new Transaction
+            {
+               TransactionPlace = "Transaction place"
+            };
+
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            var valid = Validator.TryValidateObject(model, context, result, true);
+
+
+            Assert.True(valid);
+        }
+
+        [Fact]
+        public void GivenValidModelWithTooLongTransactionPlaceShouldNotValidate()
+        {
+
+            var model = new Transaction
+            {
+                TransactionPlace = "Transaction place transaction place"
+            };
+
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            var valid = Validator.TryValidateObject(model, context, result, true);
+
+
+            Assert.False(valid);
         }
     }
 }
